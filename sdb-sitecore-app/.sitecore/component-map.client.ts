4e12bec33ce20600c26c4a29909b74724e6b5a27
Wittawat_F <wittawat.f@softdebut.com>
2026-01-28 13:26:44 +0700

    feat: add SiteInfoWidget to component map for client and server

--- sdb-sitecore-app/.sitecore/component-map.client.ts
+++ sdb-sitecore-app/.sitecore/component-map.client.ts
@@ -3,6 +3,7 @@
 import { BYOCClientWrapper, NextjsContentSdkComponent, FEaaSClientWrapper } from '@sitecore-content-sdk/nextjs';
 import { Form } from '@sitecore-content-sdk/nextjs';
 
+import * as SiteInfoWidget from 'src/components/test-graphql/SiteInfoWidget';
 import * as ProductList from 'src/components/products/ProductList';
 import * as Navigation from 'src/components/navigation/Navigation';
 
@@ -10,6 +11,7 @@ export const componentMap = new Map<string, NextjsContentSdkComponent>([
   ['BYOCWrapper', BYOCClientWrapper],
   ['FEaaSWrapper', FEaaSClientWrapper],
   ['Form', Form],
+  ['SiteInfoWidget', { ...SiteInfoWidget }],
   ['ProductList', { ...ProductList }],
   ['Navigation', { ...Navigation }],
 ]);
--- sdb-sitecore-app/.sitecore/component-map.ts
+++ sdb-sitecore-app/.sitecore/component-map.ts
@@ -5,6 +5,7 @@ import { Form } from '@sitecore-content-sdk/nextjs';
 
 // end of built-in components
 import * as Title from 'src/components/title/Title';
+import * as SiteInfoWidget from 'src/components/test-graphql/SiteInfoWidget';
 import * as RowSplitter from 'src/components/row-splitter/RowSplitter';
 import * as RichText from 'src/components/rich-text/RichText';
 import * as Promo from 'src/components/promo/Promo';
@@ -31,6 +32,7 @@ export const componentMap = new Map<string, NextjsContentSdkComponent>([
   ['FEaaSWrapper', FEaaSServerWrapper],
   ['Form', Form],
   ['Title', { ...Title }],
+  ['SiteInfoWidget', { ...SiteInfoWidget, componentType: 'client' }],
   ['RowSplitter', { ...RowSplitter }],
   ['RichText', { ...RichText }],
   ['Promo', { ...Promo }],
